f947d562f8cbfb3dee7fce48493ebfefc7fb3270
zielisgabriel <josegabrielas0586@email.com>
2026-02-22 19:47:59 -0300

    feat(translate): update translations for sign in and sign up phrases

--- web/flyer_application/src/utils/auth-form-translate.ts
+++ web/flyer_application/src/utils/auth-form-translate.ts
@@ -1,8 +1,11 @@
 const translations: Record<string, string> = {
   "ID": "Email ou Username",
   "Password": "Senha",
-  "Sign in with password": "Entrar com senha",
-  "Sign up": "Cadastrar"
+  "Sign in with password": "Entrar",
+  "Sign up": "Cadastrar",
+  "E-Mail": "E-mail",
+  "Username": "Nome de usuário",
+  "Sign up with password": "Cadastrar",
 }
 
 export function getTranslate(text: string | undefined) {
