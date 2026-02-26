22398fde06ffefacaec02c61541579852bf5c5da
zielisgabriel <josegabrielas0586@email.com>
2026-02-26 20:38:57 -0300

    feat: remove unused login, home, and register pages

--- web/flyer_application/src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/login-form"
-
-export default function LoginPage() {
-  return (
-    <main className="flex items-center justify-center h-screen">
-      <LoginForm />
-    </main>
-  )
-}
--- web/flyer_application/src/app/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import { RegisterForm } from "@/components/register-form"
-
-export default function RegisterPage() {
-  return (
-    <main className="flex items-center justify-center h-screen">
-      <RegisterForm />
-    </main>
-  )
-}
