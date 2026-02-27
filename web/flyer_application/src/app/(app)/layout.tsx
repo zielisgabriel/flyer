f40b2012a57d3b71c68b4499cbd09a9f0c293a69
zielisgabriel <josegabrielas0586@email.com>
2026-02-26 21:00:05 -0300

    feat(layout): create AppLayout component to render children with a header

---
+++ web/flyer_application/src/app/(app)/layout.tsx
@@ -0,0 +1,14 @@
+import { Header } from "@/components/header";
+
+export default function AppLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      { children }
+    </>
+  )
+}
