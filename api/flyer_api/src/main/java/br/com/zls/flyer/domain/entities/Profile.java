a78c69de052ae22502e8fef9975e03b323984f87
zielisgabriel <josegabrielas0586@email.com>
2026-03-08 14:10:23 -0300

    feat(entity): update Profile class to use BirthdayDate value object for birthdayDate field

--- api/flyer_api/src/main/java/br/com/zls/flyer/domain/entities/Profile.java
+++ api/flyer_api/src/main/java/br/com/zls/flyer/domain/entities/Profile.java
@@ -1,16 +1,19 @@
 package br.com.zls.flyer.domain.entities;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import br.com.zls.flyer.domain.valueobjects.BirthdayDate;
+import lombok.Data;
+
+@Data
 public class Profile {
   private UUID id;
   private String authId;
   private String bio;
   private String avatarUrl;
   private String firstName;
-  private LocalDate birthdayDate;
+  private BirthdayDate birthdayDate;
   private String lastName;
   private String username;
   private LocalDateTime createdAt;
